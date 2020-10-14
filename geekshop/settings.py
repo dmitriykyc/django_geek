b137595e439a7ebb21e131cf13b0e5216a89f4ac
dmitriykyc <dmitriykyc@gmail.com>
2020-10-14 23:20:23 +0300

    Lesson 4. Auth app and user model

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authnapp',
 ]
+
+# AUTH model
+AUTH_USER_MODEL = 'authnapp.ShopUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
